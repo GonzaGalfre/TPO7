1d3408a1fcc921af8303ac2a4520bb90c237f12c
GonzaGalfre <gonzaa.galfre@gmail.com>
2023-05-01 16:12:44 -0300

    migrated project from intellij idea to netbeans

--- TP7/src/main/java/com/mycompany/tp7/Main.java
+++ TP7/src/main/java/com/mycompany/tp7/Main.java
@@ -3,6 +3,5 @@ public class Main {
         Alumno alumno = new Alumno(123, "juan", "juan");
         Materia materia = new Materia(1, "matematica", 1);
         alumno.agregarMateria(materia);
-        alumno.agregarMateria(materia);
     }
 }
